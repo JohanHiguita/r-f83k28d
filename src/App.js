f5ec96909e26615d6e988d85b3c2676c0e9128ce
Johan Higuita <higuitajohan@gmail.com>
2019-02-23 21:17:46 -0500

    test passed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()  
+    this.state = {
+      counter: 0
+    }
+    
+    this.increment = this.increment.bind(this);
+  }
+  
+
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.increment}>Incrementa</button>
       </div>
     );
+  }
+
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
   }
 }
 
